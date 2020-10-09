3856ad1c32279cebfae83b67b6f7bfea87c91b74
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-10-09 13:02:08 +1000

    Update 2020-10-09T03:01:56.765Z

--- src/assets/js/modules/PageHeader.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('click', function(e) {
-
-  if (e.target && e.target.id === 'mobileNavOpen') {
-    this.documentElement.classList.add('page--mobile-open');
-  } else if (e.target && e.target.id === 'mobileNavClose') {
-    this.documentElement.classList.remove('page--mobile-open');
-  }
-});
